b687ee560fbc617589000c8d2162e9eb8a84f02c
David Carr <davidc1986@gmail.com>
2022-03-07 14:42:50 +0000

    Remove scalars

--- app/src/main/java/uk/co/peakdev/news/di/ApiModule.kt
+++ app/src/main/java/uk/co/peakdev/news/di/ApiModule.kt
@@ -6,7 +6,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import uk.co.peakdev.news.data.api.NewsApi
 import javax.inject.Singleton
 
@@ -19,7 +18,6 @@ object ApiModule {
     fun providesNewsApi(): NewsApi {
         return Retrofit.Builder()
             .baseUrl("https://newsapi.org")
-            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(NewsApi::class.java)
