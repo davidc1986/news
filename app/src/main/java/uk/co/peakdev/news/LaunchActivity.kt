c77cbd202a7d2d0153002d3fe48ff577341f027d
David Carr <davidc1986@gmail.com>
2022-03-07 10:41:49 +0000

    Get started with hilt

--- app/src/main/java/uk/co/peakdev/news/LaunchActivity.kt
+++ app/src/main/java/uk/co/peakdev/news/LaunchActivity.kt
@@ -2,7 +2,9 @@ package uk.co.peakdev.news
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LaunchActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
