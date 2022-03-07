6b4f4b4e3e7296011aae7157d6cfb833a9db1918
David Carr <davidc1986@gmail.com>
2022-03-07 10:28:04 +0000

    Initial project setup

---
+++ app/src/main/java/uk/co/peakdev/news/LaunchActivity.kt
@@ -0,0 +1,14 @@
+package uk.co.peakdev.news
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_launch)
+    }
+
+}
