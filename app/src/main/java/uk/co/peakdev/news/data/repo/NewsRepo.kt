dfc837eb8c84d340f27a46db6a291948484e29e0
David Carr <davidc1986@gmail.com>
2022-11-03 11:17:44 +0000

    Coroutine dispatcher not required for Retrofit suspend functions

--- app/src/main/java/uk/co/peakdev/news/data/repo/NewsRepo.kt
+++ app/src/main/java/uk/co/peakdev/news/data/repo/NewsRepo.kt
@@ -1,23 +1,17 @@
 package uk.co.peakdev.news.data.repo
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
+import uk.co.peakdev.news.Result
 import uk.co.peakdev.news.data.api.NewsApi
 import uk.co.peakdev.news.data.api.mapToResult
 import uk.co.peakdev.news.data.api.model.HeadlinesResponse
-import uk.co.peakdev.news.di.IoDispatcher
-import uk.co.peakdev.news.Result
 import javax.inject.Inject
 
 class NewsRepo @Inject constructor(
-    private val newsApi: NewsApi,
-    @IoDispatcher private val dispatcher: CoroutineDispatcher
+    private val newsApi: NewsApi
 ) {
 
     suspend fun fetchHeadlines(): Result<HeadlinesResponse> {
-        return withContext(dispatcher) {
-            newsApi.fetchHeadlines().mapToResult()
-        }
+        return newsApi.fetchHeadlines().mapToResult()
     }
 
 }
